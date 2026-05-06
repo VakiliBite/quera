beadb5b41a1d246f0218121aa5ea21c92bc3c817
AmirAli <54313410+identity-lo@users.noreply.github.com>
2026-05-06 08:46:10 +0330

    https://quera.org/problemset/87176 roz 11 hal tamrin

---
+++ The second seven days/11/source.py
@@ -0,0 +1,17 @@
+def game(number):
+    result = 0
+    int_list = [i for i in str(number)]
+    pervios_num = 0
+    
+    for i in int_list:
+        if pervios_num == 0:
+            pervios_num = i
+        else:
+            result = abs(int(i) - int(pervios_num))
+        
+    
+    
+    return result
+
+
+print(game(81))
