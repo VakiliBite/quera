c3f4470dad9ce3807a6f553eebff7b1416629e53
AmirAli <54313410+identity-lo@users.noreply.github.com>
2026-07-07 10:35:15 +0330

    https://quera.org/problemset/102250 roz 72 hal tamrin

---
+++ eleventhweek/72/Initial_project/peydayesh.py
@@ -0,0 +1,12 @@
+def find(num1, num2, num3):
+    result = [1 ,2 ,3 ,4]
+    parml = [num1 , num2 , num3]
+    for i in parml:
+        result.remove(i)
+
+    rr = result[0]
+    return rr
+
+
+        
+        
